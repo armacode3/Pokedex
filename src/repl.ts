67d7b1b15f4748de3caad4ea94e28c6f03bf165f
armacode3 <zavaarmando0@gmail.com>
2026-01-07 21:17:19 -0500

    Implement REPL test and loop

--- src/repl.ts
+++ src/repl.ts
@@ -1,3 +1,5 @@
+import * as readline from 'readline';
+
 export function cleanInput(input: string): string[] {
     const cleanInput = input.trim();
     const words: string[] = cleanInput.split(" ");
@@ -8,4 +10,27 @@ export function cleanInput(input: string): string[] {
         words[i] = word;
     }
     return words.filter(Boolean);
+}
+
+export function startREPL() {
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+        terminal: true,
+    });
+
+    const promptString = "Pokedex > ";
+    rl.setPrompt(promptString);
+
+    rl.prompt();
+
+    rl.on("line", (input) => {
+        const inputs = cleanInput(input);
+        if (inputs.length === 0) {
+            rl.prompt();
+            return;
+        }
+        console.log(`Your command was: ${inputs[0]}`);
+        rl.prompt();
+    });
 }
